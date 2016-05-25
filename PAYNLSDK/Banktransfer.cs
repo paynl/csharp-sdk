b3862c66e195e98ae642562b7c1ed18f47a05848
maarten <maarten@picturepresent.nl>
2016-05-25 14:15:32 +0200

    Added banktransfer and added nuget package for Newtonsoft.Json

---
+++ PAYNLSDK/Banktransfer.cs
@@ -0,0 +1,27 @@
+﻿using PAYNLSDK.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAYNLSDK
+{
+    public class Banktransfer
+    {
+        static public API.Banktransfer.Add.Response Add(PAYNLSDK.API.Banktransfer.Add.Request request)
+        {
+            Client c = new Client("", "");
+            c.PerformRequest(request);
+            return request.Response;
+        }
+
+        static public API.Banktransfer.Add.Response Add(int amount, string bankAccountHolder, string bankAccountNumber, string bankAccountBic)
+        {
+            Client c = new Client("", "");
+            var request = new API.Banktransfer.Add.Request(amount, bankAccountHolder, bankAccountNumber, bankAccountBic);
+            c.PerformRequest(request);
+            return request.Response;
+        }
+    }
+}
