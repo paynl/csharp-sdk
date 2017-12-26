3aed842a071fead70f6a6adafecc234a8de2cf73
Damiaan <damiaan@comm-it.be>
2017-12-26 14:26:42 +0100

    Added comments

--- PAYNLSDK/Enums/enums.cs
+++ PAYNLSDK/Enums/enums.cs
@@ -103,14 +103,29 @@ namespace PAYNLSDK.Enums
     /// </summary>
     public enum PaymentMethodId
     {
+        /// <summary>
+        /// Payment with SMS
+        /// </summary>
         [EnumMember(Value = "1")]
         Sms = 1,
+        /// <summary>
+        /// Payment method with fixed price
+        /// </summary>
         [EnumMember(Value = "2")]
         PayFixedPrice = 2,
+        /// <summary>
+        /// Pay per call
+        /// </summary>
         [EnumMember(Value = "3")]
         PayPerCall = 3,
+        /// <summary>
+        /// Pay per transaction
+        /// </summary>
         [EnumMember(Value = "4")]
         PayPerTransaction = 4,
+        /// <summary>
+        /// Pay per minute
+        /// </summary>
         [EnumMember(Value = "5")]
         PayPerMinute = 5
     }
