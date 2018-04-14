762e3c1952b9be0671195e20c6fe52e42e0aec11
Damiaan Peeters <damiaan@umbrace.be>
2018-04-14 15:26:31 +0200

    Only expect getters on the IPayNlConfiguration object

--- PAYNLSDK/Api/IPayNlConfiguration.cs
+++ PAYNLSDK/Api/IPayNlConfiguration.cs
@@ -1,14 +1,17 @@
 ﻿namespace PAYNLSDK.API
 {
+    /// <summary>
+    /// An object containing all configuration for 
+    /// </summary>
     public interface IPayNlConfiguration
     {
         /// <summary>
         /// The security token
         /// </summary>
-        string ApiToken { get; set; }
+        string ApiToken { get;  }
         /// <summary>
         /// The service Id,
         /// </summary>
-        string ServiceId { get; set; }
+        string ServiceId { get; }
     }
 }
