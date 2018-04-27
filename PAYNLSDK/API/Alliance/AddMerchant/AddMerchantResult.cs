3d4173c44e92b06a517d55772a2e8209875d5fd3
Damiaan Peeters <damiaan@umbrace.be>
2018-04-27 17:09:53 +0200

    Add result from Alliance/AddMerchant

--- PAYNLSDK/API/Alliance/AddMerchant/AddMerchantResult.cs
+++ PAYNLSDK/API/Alliance/AddMerchant/AddMerchantResult.cs
@@ -8,5 +8,20 @@ namespace PAYNLSDK.API.Alliance.AddMerchant
 {
     public class AddMerchantResult
     {
+
+        public string success { get; set; }
+        public string error_field { get; set; }
+        public string error_message { get; set; }
+        public string merchantId { get; set; }
+        public string merchantToken { get; set; }
+        public Account[] accounts { get; set; }
+
+
+        public class Account
+        {
+            public string accountId { get; set; }
+            public string email { get; set; }
+        }
+
     }
 }
--- PAYNLSDK/API/Alliance/AddMerchant/Request.cs
+++ PAYNLSDK/API/Alliance/AddMerchant/Request.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PAYNLSDK.API.Alliance.AddMerchant
 {
@@ -14,8 +15,88 @@ namespace PAYNLSDK.API.Alliance.AddMerchant
         public override string Method => "addMerchant";
         public override NameValueCollection GetParameters()
         {
-            var retval = new NameValueCollection { { "key", "value" } };
+            var retval = new NameValueCollection { };
+
+            retval.Add("name", FullName);
+            retval.Add("coc", Coc);
+
+            retval.Add("vat", Vat);
+            retval.Add("street", Street);
+            retval.Add("houseNumber", HouseNumber);
+            retval.Add("houseNumberAddition", HouseNumberAddition);
+            retval.Add("postalCode", PostalCode);
+            retval.Add("city", City);
+            retval.Add("countryCode", Country);
+
+            for (var i = 0; i < Accounts.Count; i++)
+            {
+                var account = Accounts[i];
+                retval.Add($"accounts[{i}]['email']", account.Email);
+                retval.Add($"accounts[{i}]['firstname']", account.FirstName);
+                retval.Add($"accounts[{i}]['lastname']", account.LastName);
+                retval.Add($"accounts[{i}]['gender']", account.Gender);
+                retval.Add($"accounts[{i}]['authorizedToSign']", account.AuthorizedToSign.ToString());
+                retval.Add($"accounts[{i}]['ubo']", account.UltimateBeneficialOwner ? "1" : "0");
+                retval.Add($"accounts[{i}]['uboPercentage']", 0.ToString());
+                retval.Add($"accounts[{i}]['useCompanyAuth']", "1");
+                retval.Add($"accounts[{i}]['hasAccess']", "1");
+                retval.Add($"accounts[{i}]['language']", "1");
+            }
+
             return retval;
+        }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string HouseNumber { get; set; }
+
+        public string HouseNumberAddition { get; set; }
+
+        public string Street { get; set; }
+
+        public string Vat { get; set; }
+
+        public string Coc { get; set; }
+
+        public string FullName { get; set; }
+
+        public List<Account> Accounts { get; set; }
+
+        public class Account
+        {
+            [JsonProperty("primary")]
+            public bool Primary { get; set; }
+
+            [JsonProperty("email")]
+            public string Email { get; set; }
+
+            [JsonProperty("firstname")]
+            public string FirstName { get; set; }
+
+            [JsonProperty("lastname")]
+            public string LastName { get; set; }
+
+            /// <summary>
+            /// "male" or "female"
+            /// </summary>
+            [JsonProperty("gender")]
+            public string Gender { get; set; }
+
+            /// <summary>
+            /// 0 not authorised, 1 authorised independently, 2  shared authorized to sign
+            /// </summary>
+            [JsonProperty("authorisedToSign")]
+            public int AuthorizedToSign { get; set; }
+
+            /// <summary>
+            /// Ultimate beneficial owner (25% of more shares)
+            /// </summary>
+            [JsonProperty("ubo")]
+            public bool UltimateBeneficialOwner { get; set; }
         }
 
         protected override void PrepareAndSetResponse()
