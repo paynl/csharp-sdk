8f1fbe1479b7da7fa4b6ad433f97bd637faf6120
Damiaan <damiaan@comm-it.be>
2019-01-29 00:26:16 +0100

    update appveyor build version patching.  We need to use the first group in case there are more groups.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 $path = (Get-Item .\PAYNLSDK\PayNLSdk.csproj).FullName
 $csproj = [xml](Get-Content $path)
-$csproj.Project.PropertyGroup.Version = $Env:APPVEYOR_BUILD_VERSION
+$csproj.Project.PropertyGroup[0].Version = $Env:APPVEYOR_BUILD_VERSION
 $csproj.Save($path)
