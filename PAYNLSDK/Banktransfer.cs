c2b82f688236e8f9981297800d93050f3ef42f85
Damiaan Peeters <damiaan@umbrace.be>
2020-07-21 16:46:55 +0200

    breaking change (bankTransfer) : change amount to decimal

--- PAYNLSDK/Banktransfer.cs
+++ PAYNLSDK/Banktransfer.cs
@@ -7,21 +7,41 @@ using System.Threading.Tasks;
 
 namespace PAYNLSDK
 {
+    /// <summary>
+    /// the Bank Transfer API
+    /// </summary>
     public class Banktransfer
     {
         private readonly IClient _webClient;
-     
+
+        /// <summary>
+        /// Create a new BankTransfer api object
+        /// </summary>
+        /// <param name="webClient"></param>
         public Banktransfer(IClient webClient)
         {
             _webClient = webClient;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public API.Banktransfer.Add.Response Add(PAYNLSDK.API.Banktransfer.Add.Request request)
         {
             _webClient.PerformRequest(request);
             return request.Response;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="bankAccountHolder"></param>
+        /// <param name="bankAccountNumber"></param>
+        /// <param name="bankAccountBic"></param>
+        /// <returns></returns>
         public API.Banktransfer.Add.Response Add(int amount, string bankAccountHolder, string bankAccountNumber, string bankAccountBic)
         {
             var request = new API.Banktransfer.Add.Request(amount, bankAccountHolder, bankAccountNumber, bankAccountBic);
